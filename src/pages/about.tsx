2e39e12ca5b670250363d4e4b631106fa8fe8b9d
azmatawan <azmatbawan@gmail.com>
2022-01-07 17:28:15 +0500

    blog site updated

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+export default () => {
+    return (
+        <div>
+            <Layout> 
+                <p>This is layout for about page </p>
+            </Layout>
+        </div>
+    );
+}
